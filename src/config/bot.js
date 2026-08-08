843a700e2cad650c6ad66047e0c63ce11eaa44d4
ya-cursed <lyblyleona207@gmail.com>
2026-08-08 13:47:05 +0200

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -141,7 +141,7 @@ export const botConfig = {
     },
     footer: {
       // Default footer text used in bot embeds.
-      text: "Titan Bot",
+      text: "Mira Bot",
       // Footer icon URL (null = no icon).
       icon: null,
     },
